e7012c8a5ca00d3104688da8934de6b4cdb3875b
Dipec001 <dpecchukwu@gmail.com>
2025-10-10 16:29:56 +0100

    `Added sensitive attribute to database_url and db_endpoint outputs in prod and staging environments.`

--- tf-infra/staging/outputs.tf
+++ tf-infra/staging/outputs.tf
@@ -1,9 +1,11 @@
 output "database_url" {
   description = "Full DATABASE_URL for Django (from RDS module)"
   value       = module.rds.database_url
+  sensitive   = true
 }
 
 output "db_endpoint" {
   description = "RDS endpoint"
   value       = module.rds.db_endpoint
+  sensitive   = true
 }
