516d41843453b972216e2ae175fe6791fb373c2b
Dipec001 <dpecchukwu@gmail.com>
2025-10-10 16:37:17 +0100

    "Added sensitive attribute to database_url output in rds module"

--- tf-infra/modules/rds/outputs.tf
+++ tf-infra/modules/rds/outputs.tf
@@ -9,5 +9,6 @@ output "db_identifier" {
 output "database_url" {
   description = "Full DATABASE_URL for Django"
   value       = "postgres://${var.db_username}:${var.db_password}@${aws_db_instance.this.endpoint}:5432/${var.db_name}"
+  sensitive   = true
 }
 
