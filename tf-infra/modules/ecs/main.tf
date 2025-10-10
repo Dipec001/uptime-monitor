ce46e401688cb76e9eee0bb6a67c8974ba7945b0
Dipec001 <dpecchukwu@gmail.com>
2025-10-10 11:48:24 +0100

    `Replace Launch Configuration with Launch Template in ECS module`

--- tf-infra/modules/ecs/main.tf
+++ tf-infra/modules/ecs/main.tf
@@ -155,23 +155,40 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
 }
 
 # =========================
-# Launch Configuration
+# Launch Template (replaces Launch Configuration)
 # =========================
+
 data "aws_ssm_parameter" "ecs_ami" {
   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
 }
 
-resource "aws_launch_configuration" "ecs_launch_config" {
-  name_prefix          = "${var.env}-ecs-lc-"
-  image_id             = data.aws_ssm_parameter.ecs_ami.value
-  instance_type        = var.ec2_instance_type
-  security_groups      = [aws_security_group.ecs_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.ecs_instance_profile.name
+resource "aws_launch_template" "ecs_launch_template" {
+  name_prefix   = "${var.env}-ecs-lt-"
+  image_id      = data.aws_ssm_parameter.ecs_ami.value
+  instance_type = var.ec2_instance_type
 
-  user_data = <<-EOF
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.ecs_sg.id]
+  }
+
+  user_data = base64encode(<<-EOF
               #!/bin/bash
               echo ECS_CLUSTER=${aws_ecs_cluster.this.name} >> /etc/ecs/ecs.config
               EOF
+  )
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.env}-ecs-instance"
+      Env  = var.env
+    }
+  }
 }
 
 # =========================
@@ -182,5 +199,8 @@ resource "aws_autoscaling_group" "ecs_asg" {
   max_size             = 2
   min_size             = 1
   vpc_zone_identifier  = var.public_subnets
-  launch_configuration = aws_launch_configuration.ecs_launch_config.id
+  launch_template {
+    id      = aws_launch_template.ecs_launch_template.id
+    version = "$Latest"
+  }
 }
