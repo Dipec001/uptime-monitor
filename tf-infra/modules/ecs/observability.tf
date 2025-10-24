89f85a1a17d09fed01e94dd7cfe22c26de598716
Dipec001 <dpecchukwu@gmail.com>
2025-10-24 16:18:08 +0100

    `Update grafana_root_url in observability.tf to include /grafana in the URL when using the load balancer DNS name.`

--- tf-infra/modules/ecs/observability.tf
+++ tf-infra/modules/ecs/observability.tf
@@ -151,7 +151,7 @@ resource "aws_ecs_task_definition" "prometheus" {
 
 # -- dynamic grafana url -----
 locals {
-  grafana_root_url = var.domain != "" ? "https://grafana.${var.domain}" : "http://${aws_lb.this.dns_name}:3000"
+  grafana_root_url = var.domain != "" ? "https://grafana.${var.domain}" : "http://${aws_lb.this.dns_name}/grafana"
 }
 
 # =======================
